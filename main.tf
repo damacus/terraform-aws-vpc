55d84fc0c63b587f5fba5c8607fa82926a8efa2a
Dan Webb <dan.webb@damacus.io>
2016-07-01 10:51:16 +0100

    Fix S3 endpoint name

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id          = "${aws_vpc.vpc.id}"
-  service_name    = "com.amazonaws.s3"
+  service_name    = "com.amazonaws.eu-west-1.s3"
   route_table_ids = ["${aws_vpc.vpc.main_route_table_id}"]
 }
