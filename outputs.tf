8ab3332ab4e22963d0e85b2f7e0dc90ff95f8833
Dan Webb <dan.webb@damacus.io>
2017-05-08 10:57:40 +0100

    pluralize subnet to make more sense

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
 
-output "public_subnet" {
+output "public_subnets" {
   value = ["${aws_subnet.public.*.id}"]
 }
 
-output "private_subnet" {
+output "private_subnets" {
   value = ["${aws_subnet.private.*.id}"]
 }
 
