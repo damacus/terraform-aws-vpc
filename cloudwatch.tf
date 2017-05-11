a981a655eea2d3df105116c15cfe75e5751e7905
Dan Webb <dan.webb@damacus.io>
2017-05-11 12:00:47 +0100

    Tidy - Fix

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,11 +1,20 @@
-resource "aws_cloudwatch_log_group" "vpc" {
-  name              = "${var.environment}-vpc-log-group"
-  retention_in_days = "${var.flow_log_retention_period}"
-}
-
-resource "aws_flow_log" "vpc" {
-  log_group_name = "${aws_cloudwatch_log_group.vpc.arn}"
-  iam_role_arn   = "${aws_iam_role.log_role.arn}"
+resource "aws_flow_log" "vpc_flow_log" {
+  log_group_name = "${aws_cloudwatch_log_group.vpc_flow_log.arn}"
+  iam_role_arn   = "${aws_iam_role.vpc_flow_log.arn}"
   vpc_id         = "${aws_vpc.vpc.id}"
   traffic_type   = "ALL"
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_log" {
+  name = "vpc_flow_log_${var.environment}"
+  retention_in_days = "${var.flow_log_retention_period}"
+
+  tags {
+    owner          = "${var.owner}"
+    Name           = "${var.environment}"
+    Description    = "${var.description}"
+    email          = "${var.email}"
+    cost_code_code = "${var.cost_code}"
+    environment    = "${var.environment}"
+  }
 }
