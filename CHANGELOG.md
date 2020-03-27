ae255520093c912e0a2b7393d7cca9650b2ca7ae
Dan Webb <dan.webb@damacus.io>
2020-03-27 11:37:28 +0000

    Allow for single NAT Gateways

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG for VPC Module
 
+# v3.0.3
+
+- Allow for a single NAT Gatway to save costs.
+
 # v3.0.2
 
 - Fix NAT Gateways to count the subnets rather than `aws_availability_zones`.
--- gateway.tf
+++ gateway.tf
@@ -1,12 +1,12 @@
 resource "aws_nat_gateway" "nat_gw" {
-  count         = length(aws_subnet.public)
+  count         = var.single_nat_gw ? 1 : length(aws_subnet.public)
   allocation_id = aws_eip.nat[1].id
   subnet_id     = aws_subnet.public[count.index].id
   tags          = merge(local.tags, { Name = "nat-gw-${terraform.workspace}" })
 }
 
 resource "aws_eip" "nat" {
-  count = length(aws_subnet.public)
+  count = var.single_nat_gw ? 1 : length(aws_subnet.public)
   vpc   = true
   tags  = merge(local.default_tags, { Name = "eip-nat-gw-${terraform.workspace}" })
 }
@@ -14,4 +14,9 @@ resource "aws_eip" "nat" {
 resource "aws_internet_gateway" "internet" {
   vpc_id = aws_vpc.vpc.id
   tags   = merge(local.tags, { "Name" = "igw_${terraform.workspace}" })
+}
+
+variable "single_nat_gw" {
+  default = false
+  type    = bool
 }
